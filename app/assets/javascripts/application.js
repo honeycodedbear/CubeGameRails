9f4ec45cb5055e4fdc2ee883aaa59643038789ce
= <=>
2015-01-19 00:07:14 -0500

    ASdf

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,11 +10,8 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require opal
-//= require opal_ujs
 //= require jquery
 //= require jquery_ujs
 //= require bootstrap-sprockets
-//= require opal-jquery
 //= require turbolinks
-//= require_tree .
+//= require pages
